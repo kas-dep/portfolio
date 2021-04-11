5e82e458eb6568e3e569798e2b0c49ec2c867287
Katarzyna Depczyńska <kasiadepczynskapl@gmail.com>
2021-04-11 20:50:17 +0200

    edit cv, add portfolio links, fix img in contact.component

--- src/app/projects/projects-data.ts
+++ src/app/projects/projects-data.ts
@@ -4,20 +4,20 @@ export const projects: Project[] = [
     id: 'myHaircarePlace',
     name: 'My Haircare Place',
     description:
-      'My haircare place jest moim głównym, najbardziej rozbudowanym i innowacyjnym projektem. Frontend aplikacji został napisany w Angular 10. W celu optymalizacji i bezpieczeństwa wykorzystałam lazy loading. Layout powstał z wykorzystaniem biblioteki Angular Material i preprocesora Less. Do backendu wykorzystałam ExpressJs i NodeJs oraz bazę danych mySQL. Frontend komunikuje się z backendem za pomocą REST API. Aplikacja umożliwia użytkownikowi rejestrację i logowanie, dodawanie, opisywanie i odczytywanie produktów, zdjęć, planowanie zabiegów pielęgnacyjncyh (FullCalendar).',
+      'My haircare place jest moim głównym, najbardziej rozbudowanym i innowacyjnym projektem. Frontend aplikacji został napisany w Angular 10. W celu optymalizacji i bezpieczeństwa wykorzystałam lazy loading. Layout powstał z wykorzystaniem biblioteki Angular Material i preprocesora Less. Aplikacja jest Desktop Only. Do backendu wykorzystałam ExpressJs i NodeJs oraz bazę danych mySQL. Frontend komunikuje się z backendem za pomocą REST API. Aplikacja umożliwia użytkownikowi rejestrację i logowanie, dodawanie, opisywanie i odczytywanie produktów, zdjęć, planowanie zabiegów pielęgnacyjncyh (FullCalendar).',
     picture: '../../../assets/images/haircarePlaceInMonitor.png',
-    url: 'http://derta.eu/gsap_animation/',
-    githubLink: 'https://github.com/kas-dep/gsap_animation ',
+    url: 'https://haircare-place.herokuapp.com/',
+    githubLink: 'https://github.com/kas-dep/haircare-place',
     areLinks: true,
   },
   {
     id: 'kasdepGame',
     name: 'Kasdep Game',
     description:
-      'Kasdep game jest grą napisaną z użyciem frameworka Angular 10. W pisaniu styli wykorzystałam Less, który pomaga tworzyć... . Logika aplikacji jest inspirowana zadaniem rekrutacyjnym. Wymogi jakie gra musi spełniać zostały opisane w instrukcji gry. Projekt ten realizowałam na początku mojej nauki Angulara, a niedawno napisałam taką samą grę z wykorzystaniem Reacta.',
+      'Kasdep game jest grą napisaną z użyciem frameworka Angular 10. W pisaniu styli wykorzystałam Less. Strona jest Desktop Only. Logika aplikacji jest inspirowana zadaniem rekrutacyjnym. Wymogi jakie gra musi spełniać zostały opisane w instrukcji gry. Projekt ten realizowałam na początku mojej nauki Angulara, a obecnie piszę taką samą grę z wykorzystaniem Reacta.',
     picture: '../../assets/images/kasdepGameMonitor.png',
-    url: 'http://derta.eu/gsap_animation/',
-    githubLink: 'https://github.com/kas-dep/gsap_animation',
+    url: 'https://kasdep-game-angular.herokuapp.com/',
+    githubLink: 'https://github.com/kas-dep/kasdep-game-angular',
     areLinks: true,
   },
   {
@@ -34,17 +34,17 @@ export const projects: Project[] = [
     id: 'portfolio',
     name: 'Portfolio',
     description:
-      'UX portfolia zaprojektowałam w programie Adobe XD, a następnie zaprogramowałam aplikację w Angular 11 z wykorzystaniem preprocesora SCSS. Widok strony jest responsywny - zastosowałam zasadę Mobile First. Portfolio ma na celu przedstawienie moich umiejętności programistycznych oraz zaprezentowanie kilku moich aplikacji.',
+      'UX portfolia zaprojektowałam w programie Adobe XD, a następnie zaprogramowałam aplikację w Angular 11 z wykorzystaniem preprocesora SCSS. Widok strony jest responsywny- zastosowałam zasadę Mobile First. Portfolio ma na celu przedstawienie moich umiejętności programistycznych oraz zaprezentowanie kilku moich aplikacji.',
     picture: '../../assets/images/portfolioInMonitor.png',
-    url: 'http://derta.eu/gsap_animation/',
-    githubLink: 'https://github.com/kas-dep/gsap_animation',
+    url: 'https://katarzyna-depczynska-portfolio.herokuapp.com/',
+    githubLink: 'https://github.com/kas-dep/portfolio',
     areLinks: true,
   },
   {
     id: 'animation',
     name: 'Animacja Flower for you',
     description:
-      'Prosta aplikacja stworzona w celu praktycznej nauki GSAP Animations. Do jej zaprogramowania wykorzystałam HTML5, CSS3 oraz Javascript ES6. Projekt jest na tyle mały, że nie wymagał użycia innych bibliotek czy frameworków. ',
+      'Prosta aplikacja stworzona w celu praktycznej nauki GSAP Animations. Do jej zaprogramowania wykorzystałam HTML5, CSS3 oraz Javascript ES6. Widok strony jest responsywny- zastosowałam zasadę Mobile First. Projekt jest na tyle mały, że nie wymagał użycia innych bibliotek czy frameworków. ',
     picture: '../../assets/images/flowerForYouMonitor.png',
     url: 'https://simple-gsap-animation.herokuapp.com/',
     githubLink: 'https://github.com/kas-dep/gsap_animation',
